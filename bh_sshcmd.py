a6fbc13bde0165e2c7df5d34dd243c8688d62f96
Cody Sparky Wood <codyrswood@gmail.com>
2015-01-02 21:23:36 +0000

    ssh check

--- bh_sshcmd.py
+++ bh_sshcmd.py
@@ -1,3 +1,16 @@
 import threading
-import paramiko
+import paramiko#ssh client http://www.paramiko.org
 import subprocess
+
+def ssh_command(ip, user, passwd, command):
+        client = paramiko.SSHClient()
+        #client.load_host_keys('/home/user/.ssh/known_hosts')
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(ip, username=user, password=passwd)
+        ssh_session = client.get_transport().open_session()
+        if ssh_session.active:
+            ssh_session.exec_command(command)
+            print ssh_session.recv(1024)
+        return
+    
+#ssh_command('192.168.100.100', 'dude', 1337hax0r', 'id')
